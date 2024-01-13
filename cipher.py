716512c5e42c68184ff8fb771aa70e04f8b9adcc
bandera002 <156542683+bandera002@users.noreply.github.com>
2024-01-13 21:46:48 +0000

    updated Artem

--- cipher.py
+++ cipher.py
@@ -1,7 +1,7 @@
 message = input("Enter your message: ")
 key = input("Enter your key: ")
-operation = input ("operation, type D/d for description ")
-key = int (key) %26
+operation = input("operation, type D/d for decryption ")
+key = int(key) % 26
 if operation == "D" or operation == "d":
     key = key * -1
 lower_a_val= ord('a')
@@ -9,17 +9,17 @@ upper_a_val = ord('A')
 
 letters =  list()
 for char in message:
-int_val = ord (char)
-new_char = int_val
-if int_val >= upper_a_val and int_val < upper_a_val+26:
-new_char = ord(char) + int(key)
-if new_char > lower_a_val+26:
-    new_char = new_char - 26
+    int_val = ord(char)
+    new_char = int_val
+    if int_val >= lower_a_val and int_val < lower_a_val+26:
+        new_char = ord(char) + int(key)
+        if new_char > lower_a_val+26:
+            new_char = new_char - 26
     if int_val >= upper_a_val and int_val < upper_a_val+26:
         new_char = ord(char) + int(key)
-        if new char > upper_a_val + 26:
-            new_char = new_char - 26
-            letters.append(chr(new_char))
+        if new_char > upper_a_val + 26:
+            new_char =  new_char - 26
+    letters.append(chr(new_char))
 
-            phrase = ''.join(letters) 
-            print(message, key, phrase)
+phrase = ''.join(letters)
+print(message, key , phrase)
